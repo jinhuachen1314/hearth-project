b0fb4231f128039b1ede912a32fc4ed3804ba02c
Jin Chen <sagittariusjchen@gmail.com>
2021-11-26 00:40:43 -0800

    add comment

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const app = express();
 const HouseList = require('./HouseList');
-
 const list = new HouseList();
 
+/**
+ * Initialize house list.
+ */
 app.use(async (req, res, next) => {
   if (list.isEmpty()) list.getHousesFromCSV();
   next();
 });
 
-// Get addresses of the houses.
+/**
+ * Get addresses of the houses.
+ */
 app.get('/address', (req, res) => {
-  console.log("RUN HERE");
   const addresses = list.houses.map(house => house["ADDRESS"]);
   res.send(addresses);
 });
 
+/**
+ * House search endpoint.
+ */
 app.get('/search', (req, res) => {
   const { address } = req.query;
-
   const house = list.houses.find(house => {
     return house.ADDRESS === address;
   });
